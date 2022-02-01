ec1c0127668a7ba40fbd596a1537588031483fc4
Alex Kelly <kellya@arachnitech.com>
2022-02-01 10:22:18 -0500

    add /project as a valid entry point for latest

--- hostthedocs/__init__.py
+++ hostthedocs/__init__.py
@@ -3,64 +3,69 @@ import os
 from flask import abort, Flask, jsonify, redirect, render_template, request
 
 from . import getconfig, util
-from .filekeeper import delete_files, insert_link_to_latest, parse_docfiles, unpack_project
+from .filekeeper import (
+    delete_files,
+    insert_link_to_latest,
+    parse_docfiles,
+    unpack_project,
+)
 
 app = Flask(__name__)
 
-app.config['MAX_CONTENT_LENGTH'] = getconfig.max_content_mb * 1024 * 1024
+app.config["MAX_CONTENT_LENGTH"] = getconfig.max_content_mb * 1024 * 1024
 
 
-@app.route('/hmfd', methods=['POST', 'DELETE'])
+@app.route("/hmfd", methods=["POST", "DELETE"])
 def hmfd():
     if getconfig.readonly:
         return abort(403)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         if not request.files:
-            return abort(400, 'Request is missing a zip/tar file.')
+            return abort(400, "Request is missing a zip/tar file.")
         uploaded_file = util.file_from_request(request)
-        unpack_project(
-            uploaded_file,
-            request.form,
-            getconfig.docfiles_dir
-        )
+        unpack_project(uploaded_file, request.form, getconfig.docfiles_dir)
         uploaded_file.close()
-    elif request.method == 'DELETE':
+    elif request.method == "DELETE":
         if getconfig.disable_delete:
             return abort(403)
 
         delete_files(
-            request.args['name'],
-            request.args.get('version'),
+            request.args["name"],
+            request.args.get("version"),
             getconfig.docfiles_dir,
-            request.args.get('entire_project'))
+            request.args.get("entire_project"),
+        )
     else:
         abort(405)
 
-    return jsonify({'success': True})
+    return jsonify({"success": True})
 
 
-@app.route('/')
+@app.route("/")
 def home():
     projects = parse_docfiles(getconfig.docfiles_dir, getconfig.docfiles_link_root)
-    insert_link_to_latest(projects, '%(project)s/latest')
-    return render_template('index.html', projects=projects, **getconfig.renderables)
+    insert_link_to_latest(projects, "%(project)s/latest")
+    return render_template("index.html", projects=projects, **getconfig.renderables)
 
 
-@app.route('/<project>/latest/')
+@app.route("/<project>")
+@app.route("/<project>/latest/")
 def latest_root(project):
-    return latest(project, '')
+    return latest(project, "")
 
 
-@app.route('/<project>/latest/<path:path>')
+@app.route("/<project>/latest/<path:path>")
 def latest(project, path):
-    parsed_docfiles = parse_docfiles(getconfig.docfiles_dir, getconfig.docfiles_link_root)
-    proj_for_name = dict((p['name'], p) for p in parsed_docfiles)
+    parsed_docfiles = parse_docfiles(
+        getconfig.docfiles_dir, getconfig.docfiles_link_root
+    )
+    proj_for_name = dict((p["name"], p) for p in parsed_docfiles)
     if project not in proj_for_name:
-        return 'Project %s not found' % project, 404
-    latestindex = proj_for_name[project]['versions'][-1]['link']
+        return "Project %s not found" % project, 404
+    latestindex = proj_for_name[project]["versions"][-1]["link"]
     if path:
-        latestlink = '%s/%s' % (os.path.dirname(latestindex), path)
+        latestlink = "%s/%s" % (os.path.dirname(latestindex), path)
     else:
         latestlink = latestindex
-    return redirect('/' + latestlink)
+    return redirect("/" + latestlink)
